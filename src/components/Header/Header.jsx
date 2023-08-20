2866f1be137cc5a31f094457145556648b5bd206
mdyasin111 <mdyasin7636@gmail.com>
2023-08-20 17:33:46 +0600

    header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,27 @@
-import { Container } from "@mui/material";
+import { Box, Button, Container, IconButton, Stack } from "@mui/material";
+import logoImg from "../../assets/logo.png";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { FlexBox } from "../styled/FlexBox";
 
 const Header = () => {
   return (
     <div>
-      <Container maxWidth='lg'>
-        <h1>Header</h1>
+      <Container maxWidth="lg">
+        <FlexBox justifyContent='space-between'>
+          <img
+            src={logoImg}
+            alt="Tasty Tales Logo"
+            style={{ width: "160px" }}
+          />
+
+          <Stack direction='row' spacing={4}>
+            <IconButton>
+              <ShoppingCartOutlinedIcon />
+            </IconButton>
+            <Button>Sign In</Button>
+            <Button>Sign Up</Button>
+          </Stack>
+        </FlexBox>
       </Container>
     </div>
   );
---
+++ src/components/styled/FlexBox.js
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+
+export const FlexBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+}));
